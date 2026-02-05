c9dbfb1e502e295463830ecf11de867a099afd48
Mihir <Mihir@admins-MacBook-Pro.local>
2026-02-05 10:37:45 +0100

    test: tmk MODBUS control integration test

--- integration/conftest.py
+++ integration/conftest.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import time
 
 import pytest
 
@@ -8,14 +9,22 @@ import pytest
 def tmk_application():
     """Fixture to start the TMK application before tests and stop it after tests."""
     env = os.environ.copy()
-    env["TMK_CONTROLLER"] = "http"
+    # Enable HTTP controller
+    env["TMK_CONTROLLERS_HTTP_ENABLED"] = "true"
     env["TMK_CONTROLLERS_HTTP_ADDR"] = ":8080"
+
+    # Enable Modbus controller
+    env["TMK_CONTROLLERS_MODBUS_ENABLED"] = "true"
+    env["TMK_CONTROLLERS_MODBUS_ADDR"] = "0.0.0.0:1502"
+    env["TMK_CONTROLLERS_MODBUS_UNIT_ID"] = "4"
+
+    # Regulator settings
     env["TMK_REGULATOR_MODE_CHANGE_HYSTERESIS"] = "2.0"
     env["TMK_REGULATOR_TARGET_HYSTERESIS"] = "1.0"
     process = subprocess.Popen(
         ["./.bin/thermocktat"], env=env, stdout=None, stderr=None
     )
-
+    time.sleep(2)
     yield  # Tests will run after this point
 
     process.terminate()
